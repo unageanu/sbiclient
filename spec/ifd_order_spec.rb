f21c36d81c480b58c7d5c28a0a03fdb732abc710
unageanu <y-masaya@red.hot.co.jp>
2009-11-15 11:41:48 +0900

    テストケースがコメントアウトされたままだったのを修正。

--- spec/ifd_order_spec.rb
+++ spec/ifd_order_spec.rb
@@ -6,30 +6,30 @@ require 'common'
 describe "IFD" do
   it_should_behave_like "login"   
   
-#  it "指値-指値" do
-#    @order_id = @s.order( SBIClient::FX::EURJPY, SBIClient::FX::BUY, 1, {
-#      :rate=>@rates[SBIClient::FX::EURJPY].ask_rate - 0.5,
-#      :execution_expression=>SBIClient::FX::EXECUTION_EXPRESSION_LIMIT_ORDER,
-#      :expiration_type=>SBIClient::FX::EXPIRATION_TYPE_TODAY,
-#      :settle => {
-#        :rate=>@rates[SBIClient::FX::EURJPY].ask_rate + 0.5,
-#        :execution_expression=>SBIClient::FX::EXECUTION_EXPRESSION_LIMIT_ORDER
-#      }
-#    })
-#    orders = @s.list_orders
-#    @order = orders[@order_id.order_no]
-#    @order_id.should_not be_nil
-#    @order_id.order_no.should_not be_nil
-#    @order.should_not be_nil
-#    @order.order_no.should == @order_id.order_no
-#    @order.trade_type.should == SBIClient::FX::TRADE_TYPE_NEW
-#    @order.execution_expression.should == SBIClient::FX::EXECUTION_EXPRESSION_LIMIT_ORDER
-#    @order.sell_or_buy.should == SBIClient::FX::BUY
-#    @order.pair.should == SBIClient::FX::EURJPY
-#    @order.count.should == 1
-#    @order.rate.should == @rates[SBIClient::FX::EURJPY].ask_rate - 0.5
-#    @order.order_type= SBIClient::FX::ORDER_TYPE_IFD
-#  end
+  it "指値-指値" do
+    @order_id = @s.order( SBIClient::FX::EURJPY, SBIClient::FX::BUY, 1, {
+      :rate=>@rates[SBIClient::FX::EURJPY].ask_rate - 0.5,
+      :execution_expression=>SBIClient::FX::EXECUTION_EXPRESSION_LIMIT_ORDER,
+      :expiration_type=>SBIClient::FX::EXPIRATION_TYPE_TODAY,
+      :settle => {
+        :rate=>@rates[SBIClient::FX::EURJPY].ask_rate + 0.5,
+        :execution_expression=>SBIClient::FX::EXECUTION_EXPRESSION_LIMIT_ORDER
+      }
+    })
+    orders = @s.list_orders
+    @order = orders[@order_id.order_no]
+    @order_id.should_not be_nil
+    @order_id.order_no.should_not be_nil
+    @order.should_not be_nil
+    @order.order_no.should == @order_id.order_no
+    @order.trade_type.should == SBIClient::FX::TRADE_TYPE_NEW
+    @order.execution_expression.should == SBIClient::FX::EXECUTION_EXPRESSION_LIMIT_ORDER
+    @order.sell_or_buy.should == SBIClient::FX::BUY
+    @order.pair.should == SBIClient::FX::EURJPY
+    @order.count.should == 1
+    @order.rate.should == @rates[SBIClient::FX::EURJPY].ask_rate - 0.5
+    @order.order_type= SBIClient::FX::ORDER_TYPE_IFD
+  end
 
   it "指値-逆指値" do
     @order_id = @s.order( SBIClient::FX::USDJPY, SBIClient::FX::SELL, 1, {
